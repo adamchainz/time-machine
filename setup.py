3e89c4f485ad228dca5fea7d7a3b8de731555a5a
Adam Johnson <me@adamj.eu>
2020-04-29 11:13:01 +0100

    👨‍🎨black

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import Extension, setup
 
-setup(
-    ext_modules=[
-        Extension(
-            name="_tachyon_gun",
-            sources=["src/_tachyon_gun.c"],
-        )
-    ],
-)
+setup(ext_modules=[Extension(name="_tachyon_gun", sources=["src/_tachyon_gun.c"])])
