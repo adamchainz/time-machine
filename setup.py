742d60965ea9ffd013f178ee9309402c52e74693
Adam Johnson <me@adamj.eu>
2020-04-29 07:18:26 +0100

    Version for registration

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import Extension, setup
 
 setup(
     name="tachyongun",
-    version="1.0.0",
-    description="uh",
-
+    version="0.0.1",
+    description="Faster test time warping",
     zip_safe=False,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
     py_modules=["tachyongun"],
