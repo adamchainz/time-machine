9033ea155f15a64d568ad506212a1fa993af6185
Adam Johnson <me@adamj.eu>
2020-05-08 10:36:09 +0100

    💁‍♂️ Alphabetically sort time function references

--- tests/test_time_machine.py
+++ tests/test_time_machine.py
@@ -79,28 +79,28 @@ def test_date_today():
 # time module
 
 
-def test_time_time():
+def test_time_gmtime_no_args():
     with time_machine.travel(EPOCH):
-        assert EPOCH < time.time() < EPOCH + 1.0
-    assert time.time() >= LIBRARY_EPOCH
+        local_time = time.gmtime()
+        assert local_time.tm_year == 1970
+        assert local_time.tm_mon == 1
+        assert local_time.tm_mday == 1
+    now_time = time.gmtime()
+    assert now_time.tm_year >= 2020
 
 
-def test_time_time_no_tick():
+def test_time_gmtime_no_args_no_tick():
     with time_machine.travel(EPOCH, tick=False):
-        assert time.time() == EPOCH
+        local_time = time.gmtime()
+        assert local_time.tm_sec == 0
 
 
-@py_3_7_plus
-def test_time_time_ns():
+def test_time_gmtime_arg():
     with time_machine.travel(EPOCH):
-        assert int(EPOCH * 1_000_000) < time.time_ns() < 1_000_000
-    assert time.time_ns() >= int(LIBRARY_EPOCH * 1_000_000)
-
-
-@py_3_7_plus
-def test_time_time_ns_no_tick():
-    with time_machine.travel(EPOCH, tick=False):
-        assert time.time_ns() == int(EPOCH * 1_000_000)
+        local_time = time.gmtime(EPOCH_PLUS_ONE_YEAR)
+        assert local_time.tm_year == 1971
+        assert local_time.tm_mon == 1
+        assert local_time.tm_mday == 1
 
 
 def test_time_localtime():
@@ -127,30 +127,6 @@ def test_time_localtime_arg():
         assert local_time.tm_mday == 1
 
 
-def test_time_gmtime_no_args():
-    with time_machine.travel(EPOCH):
-        local_time = time.gmtime()
-        assert local_time.tm_year == 1970
-        assert local_time.tm_mon == 1
-        assert local_time.tm_mday == 1
-    now_time = time.gmtime()
-    assert now_time.tm_year >= 2020
-
-
-def test_time_gmtime_no_args_no_tick():
-    with time_machine.travel(EPOCH, tick=False):
-        local_time = time.gmtime()
-        assert local_time.tm_sec == 0
-
-
-def test_time_gmtime_arg():
-    with time_machine.travel(EPOCH):
-        local_time = time.gmtime(EPOCH_PLUS_ONE_YEAR)
-        assert local_time.tm_year == 1971
-        assert local_time.tm_mon == 1
-        assert local_time.tm_mday == 1
-
-
 def test_time_strftime_no_args():
     with time_machine.travel(EPOCH):
         assert time.strftime("%Y-%m-%d") == "1970-01-01"
@@ -168,6 +144,30 @@ def test_time_strftime_arg():
             time.strftime("%Y-%m-%d", time.localtime(EPOCH_PLUS_ONE_YEAR))
             == "1971-01-01"
         )
+
+
+def test_time_time():
+    with time_machine.travel(EPOCH):
+        assert EPOCH < time.time() < EPOCH + 1.0
+    assert time.time() >= LIBRARY_EPOCH
+
+
+def test_time_time_no_tick():
+    with time_machine.travel(EPOCH, tick=False):
+        assert time.time() == EPOCH
+
+
+@py_3_7_plus
+def test_time_time_ns():
+    with time_machine.travel(EPOCH):
+        assert int(EPOCH * 1_000_000) < time.time_ns() < 1_000_000
+    assert time.time_ns() >= int(LIBRARY_EPOCH * 1_000_000)
+
+
+@py_3_7_plus
+def test_time_time_ns_no_tick():
+    with time_machine.travel(EPOCH, tick=False):
+        assert time.time_ns() == int(EPOCH * 1_000_000)
 
 
 # other usage
