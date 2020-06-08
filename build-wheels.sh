22a92a9a126489501dfc352ed0609823f39bbf21
Adam Johnson <me@adamj.eu>
2020-06-08 18:12:26 +0100

    More wheel building updates. Still imperfect.

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,22 +1,36 @@
 #!/bin/sh
 
-mkdir -p wheelhouse
-rm -rf wheelhouse/*
-
 # Adapted from https://github.com/pypa/python-manylinux-demo/
+
+set -e
+
+mkdir -p wheelhouse
 
 # shellcheck disable=SC2016
 DOCKER_SCRIPT='
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" wheel /io/ --no-deps -w /io/wheelhouse/
-done'
+    "${PYBIN}/pip" wheel /io/ --no-deps -w "/io/wheelhouse/$PLAT/"
+done
 
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/$PLAT/
+    fi
+}
+
+for whl in /io/wheelhouse/$PLAT/*.whl; do
+    repair_wheel "$whl"
+    rm "$whl"
+done'
 
 docker run --rm -e PLAT=manylinux1_i686 -v "$(pwd)":/io quay.io/pypa/manylinux1_i686 linux32 bash -c "$DOCKER_SCRIPT"
 docker run --rm -e PLAT=manylinux1_x86_64 -v "$(pwd)":/io quay.io/pypa/manylinux1_x86_64 bash -c "$DOCKER_SCRIPT"
 docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd)":/io quay.io/pypa/manylinux2010_x86_64 bash -c "$DOCKER_SCRIPT"
 
-# Only manylinux wheels
-rm wheelhouse/*-linux*
+# Only singly tagged wheels
+find wheelhouse -iname '*manylinux*manylinux*' -delete
 
-twine check wheelhouse/*
+twine check wheelhouse/*/*.whl
