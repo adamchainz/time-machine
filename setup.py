52c8f1b00365942dfb9617ecc3675f2239ef81a3
Michał Górny <mgorny@gentoo.org>
2022-12-31 11:18:05 +0100

    Throw an explicit error when trying to build on PyPy (#315)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
 
+import sys
+
 from setuptools import Extension
 from setuptools import setup
+
+
+if hasattr(sys, "pypy_version_info"):
+    raise RuntimeError(
+        "PyPy is not currently supported by time-machine, see "
+        "https://github.com/adamchainz/time-machine/issues/305"
+    )
+
 
 setup(ext_modules=[Extension(name="_time_machine", sources=["src/_time_machine.c"])])
