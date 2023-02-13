6ca40f58851a72cbea7b6a6d2918ecc1465d2ad2
Steven Van Ingelgem <steven@vaningelgem.be>
2023-02-13 18:49:23 +0100

    And don't forget to set up the correct symbol as well...

--- tests/test_time_machine.py
+++ tests/test_time_machine.py
@@ -421,6 +421,7 @@ def test_destination_datetime_tzinfo_pytz(monkeypatch):
         import pytz
 
     monkeypatch.setattr(time_machine, 'HAVE_PYTZ', True)
+    monkeypatch.setattr(time_machine, 'pytz', pytz)
 
     dest = LIBRARY_EPOCH_DATETIME.replace(tzinfo=pytz.timezone("Africa/Addis_Ababa"))
     with pytest.raises(RuntimeError, match="We don't support pytz"):
