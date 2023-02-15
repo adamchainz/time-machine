bd49144e85f6034586e400e166d7daca8ea2b531
Steven Van Ingelgem <steven@vaningelgem.be>
2023-02-15 20:59:01 +0100

    Revert "Removed duplicate code"
    This reverts commit 0b2b85e1

--- requirements/compile.py
+++ requirements/compile.py
@@ -8,23 +8,37 @@ from pathlib import Path
 
 if __name__ == "__main__":
     os.chdir(Path(__file__).parent)
-
     os.environ["CUSTOM_COMPILE_COMMAND"] = "requirements/compile.py"
     os.environ["PIP_REQUIRE_VIRTUALENV"] = "0"
-
-    for python_version in ["3.7", "3.8", "3.9", "3.10", "3.11"]:
-        subprocess.run(
-            [
-                f"python{python_version}",
-                "-m",
-                "piptools",
-                "compile",
-                "--generate-hashes",
-                "--allow-unsafe",
-                *sys.argv[1:],
-                "-o",
-                f"py{python_version.replace('.', '')}.txt",
-            ],
-            check=True,
-            capture_output=True,
-        )
+    common_args = [
+        "-m",
+        "piptools",
+        "compile",
+        "--generate-hashes",
+        "--allow-unsafe",
+    ] + sys.argv[1:]
+    subprocess.run(
+        ["python3.7", *common_args, "-o", "py37.txt"],
+        check=True,
+        capture_output=True,
+    )
+    subprocess.run(
+        ["python3.8", *common_args, "-o", "py38.txt"],
+        check=True,
+        capture_output=True,
+    )
+    subprocess.run(
+        ["python3.9", *common_args, "-o", "py39.txt"],
+        check=True,
+        capture_output=True,
+    )
+    subprocess.run(
+        ["python3.10", *common_args, "-o", "py310.txt"],
+        check=True,
+        capture_output=True,
+    )
+    subprocess.run(
+        ["python3.11", *common_args, "-o", "py311.txt"],
+        check=True,
+        capture_output=True,
+    )
