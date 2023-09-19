1e3a11dfc8ab2c3d5daecc435a57403f02d9e3f4
Adam Johnson <me@adamj.eu>
2023-09-19 11:09:32 +0100

    Move and rename tests

--- tests/test_time_machine.py
+++ tests/test_time_machine.py
@@ -444,6 +444,24 @@ def test_destination_date():
     assert time.time() == EPOCH
 
 
+def test_destination_timedelta():
+    now = time.time()
+    with time_machine.travel(dt.timedelta(seconds=3600)):
+        assert now + 3600 <= time.time() <= now + 3601
+
+
+def test_destination_timedelta_negative():
+    now = time.time()
+    with time_machine.travel(dt.timedelta(seconds=-3600)):
+        assert now - 3600 <= time.time() <= now - 3599
+
+
+def test_destination_timedelta_nested():
+    with time_machine.travel(EPOCH):
+        with time_machine.travel(dt.timedelta(seconds=10)):
+            assert time.time() == EPOCH + 10.0
+
+
 @time_machine.travel("1970-01-01 00:01 +0000")
 def test_destination_string():
     assert time.time() == EPOCH + 60.0
@@ -462,24 +480,6 @@ def test_destination_callable_lambda_string():
 @time_machine.travel(EPOCH + 13.0 for _ in range(1))  # pragma: no branch
 def test_destination_generator():
     assert time.time() == EPOCH + 13.0
-
-
-def test_destination_delta():
-    now = time.time()
-    with time_machine.travel(dt.timedelta(seconds=3600)):
-        assert now + 3600 <= time.time() <= now + 3601
-
-
-def test_destination_negative_delta():
-    now = time.time()
-    with time_machine.travel(dt.timedelta(seconds=-3600)):
-        assert now - 3600 <= time.time() <= now - 3599
-
-
-def test_destination_delta_raises():
-    with time_machine.travel(EPOCH):
-        with time_machine.travel(dt.timedelta(seconds=10)):
-            assert time.time() == EPOCH + 10.0
 
 
 def test_traveller_object():
