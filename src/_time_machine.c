654879e9e894fb1175a4c7c93cd4656d31f5ce08
kingbuzzman <buzzi.javier@gmail.com>
2025-08-07 05:27:19 -0400

    .

--- src/_time_machine.c
+++ src/_time_machine.c
@@ -529,8 +529,11 @@ static struct PyModuleDef _time_machine_module = {
     .m_slots = _time_machine_slots,
     .m_traverse = NULL,
     .m_clear = NULL,
-    .m_free = NULL,
+    .m_free = NULL
+#if PY_VERSION_HEX >= 0x030C0000
+    ,
     .m_gil = Py_MOD_GIL_NOT_USED
+#endif
 };
 
 PyMODINIT_FUNC
