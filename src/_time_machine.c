725b48ae2d8450c62fe0f100ec15159b092f337e
kingbuzzman <buzzi.javier@gmail.com>
2025-08-07 05:30:40 -0400

    .

--- src/_time_machine.c
+++ src/_time_machine.c
@@ -517,6 +517,9 @@ static PyMethodDef module_functions[] = {
 };
 
 static PyModuleDef_Slot _time_machine_slots[] = {
+#if PY_VERSION_HEX >= 0x030D0000
+    {Py_mod_gil, Py_MOD_GIL_NOT_USED},
+#endif
     {0, NULL}
 };
 
