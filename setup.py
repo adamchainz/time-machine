528fdfb57f49222f627e393298bf356c67f3fa97
Adam Johnson <me@adamj.eu>
2026-05-01 22:46:43 +0100

    Build with frame pointers enabled

--- setup.py
+++ setup.py
@@ -10,5 +10,20 @@ if hasattr(sys, "pypy_version_info"):
         "https://github.com/adamchainz/time-machine/issues/305"
     )
 
+extra_compile_args = []
+if sys.platform != "win32":
+    extra_compile_args += [
+        "-fno-omit-frame-pointer",
+        "-mno-omit-leaf-frame-pointer",
+    ]
 
-setup(ext_modules=[Extension(name="_time_machine", sources=["src/_time_machine.c"])])
+
+setup(
+    ext_modules=[
+        Extension(
+            name="_time_machine",
+            sources=["src/_time_machine.c"],
+            extra_compile_args=extra_compile_args,
+        )
+    ]
+)
